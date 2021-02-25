8c1333c2240772fb4ad54e50a2ee0a0b7a8c39ee
HP <HP@LAPTOP-QP085JIU>
2021-02-25 18:23:46 +0530

    changes by ranjith

--- TestingGit/src/GitTest/Example.java
+++ TestingGit/src/GitTest/Example.java
@@ -5,6 +5,8 @@ public class Example {
 	public static void main(String[] args) {
 		System.out.println("Deekshith Naik");
 		System.out.println("Second commit");
+		System.out.println("Ranjith");
+
 
 	}
 
